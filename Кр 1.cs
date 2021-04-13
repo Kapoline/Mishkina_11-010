ed3ec8290760babe798f6d5240d379e40905c5cc
Polina <80746085+Kapoline@users.noreply.github.com>
2021-04-13 11:46:47 +0300

    Add files via upload

---
+++ Кр 1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace контрольная_работа
+{
+    class SetofStacks
+    {
+        List<int> stacks { get; set; }
+        int capasity { get; set; }
+        internal int capasitys
+        {
+            set { capasity = value; }
+            get { return capasity; }
+        }
+        internal List<int> stackks
+        {
+            set { stacks = value; }
+            get { return stacks; }
+        }
+        internal SetofStacks(List<int> list)
+        {
+            this.stacks = list;
+        }
+        internal void Push(int n)
+        {
+            Stack<int> stack = new Stack<int>();
+            if (stack != null && Convert.ToInt32(stack) < capasity)
+            {
+                stack.Push(n);
+                stacks.Add(Convert.ToInt32(stack));
+            }
+            else if (stack != null && Convert.ToInt32(stack)>=capasity)
+            {
+                Stack<int> newstack = new Stack<int>();
+                newstack.Push(n);
+                stacks.Add(Convert.ToInt32(newstack));
+            }
+        }
+        internal void Pop()
+        {
+            Stack<int> stack = new Stack<int>();
+            if(stack==null)
+                throw new Exception("EmptyStackExseption");
+            int temp = stack.Pop();
+        }
+        internal SetofStacks GetSetofStacks()
+        {
+            if(this.stacks==null)
+                return null;
+            return new SetofStacks(this.stacks.Count - 1);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ кр 2.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace контрольная_работа_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>();
+            int capasity = int.Parse(Console.ReadLine());
+            Random rnd = new Random();
+            for(int i=0;i<capasity;i++)
+            {
+                list.Add(rnd.Next(0, 10));
+            }
+            foreach(int i in list)
+            {
+                Console.WriteLine(i);
+            }
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (list[i] % 2 != 0  && list[i] % 5 != 0  &&  list[i] % 3 != 0)
+                {
+                    list.Add(list[i]);
+                    int temp = list.IndexOf(list[i]);
+                    for (int j = 0; j < list.Count; j++)
+                    {
+                        if(temp==j)
+                        {
+                            int count = list[j];
+                            list[j] = list[i];
+                            list[i] = count;
+                        }
+                    }   
+                }
+            }
+            foreach (int i in list)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
