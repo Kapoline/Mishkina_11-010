93fc01dda07c8515c75841f17004f97b73f75b16
Polina <80746085+Kapoline@users.noreply.github.com>
2021-05-08 19:50:03 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+
+namespace данные
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int[,] array = new int[250, 250];
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    array[i, j] = rnd.Next(1, 10);
+                }
+            }
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    Console.Write(array[i, j]);
+                }
+                Console.WriteLine("\t\n");
+            }
+            string text = @"C:\Users\User\OneDrive\Рабочий стол\семестровка\250.txt";
+            try
+            {
+                using (StreamWriter sw = new StreamWriter(text, true, System.Text.Encoding.Default))
+                {
+                    for (int i = 0; i < array.GetLength(0); i++)
+                    {
+                        for (int j = 0; j < array.GetLength(1); j++)
+                        {
+                            sw.WriteLine(array[i, j]);
+                        }
+                        Console.WriteLine("\t\n");
+                    }
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
---
+++ Семестровка.cpp
@@ -0,0 +1,80 @@
+﻿// Ford-Fulkerson algorith in C++
+
+#include <limits.h>
+#include <string.h>
+#include <fstream>
+#include <iostream>
+#include <queue>
+using namespace std;
+
+#define V 250
+
+bool bfs(int rGraph[V][V], int s, int t, int parent[]) {
+    bool visited[V];
+    memset(visited, 0, sizeof(visited));
+
+    queue<int> q;
+    q.push(s);
+    visited[s] = true;
+    parent[s] = -1;
+
+    while (!q.empty()) {
+        int u = q.front();
+        q.pop();
+
+        for (int v = 0; v < V; v++) {
+            if (visited[v] == false && rGraph[u][v] > 0) {
+                q.push(v);
+                parent[v] = u;
+                visited[v] = true;
+            }
+        }
+    }
+
+    return (visited[t] == true);
+}
+
+int fordFulkerson(int graph[V][V], int s, int t) {
+    int u, v;
+
+    int rGraph[V][V];
+    for (u = 0; u < V; u++)
+        for (v = 0; v < V; v++)
+            rGraph[u][v] = graph[u][v];
+
+    int parent[V];
+    int max_flow = 0;
+    int iteration = 0;
+
+    while (bfs(rGraph, s, t, parent)) {
+        int path_flow = INT_MAX;
+        for (v = t; v != s; v = parent[v]) {
+            u = parent[v];
+            path_flow = min(path_flow, rGraph[u][v]);
+        }
+
+        for (v = t; v != s; v = parent[v]) {
+            u = parent[v];
+            rGraph[u][v] -= path_flow;
+            rGraph[v][u] += path_flow;
+        }
+
+        max_flow += path_flow;
+        iteration++;
+    }
+    cout << iteration << endl;
+    return max_flow;
+}
+
+int main() {
+    int graph[V][V];
+    ifstream reader;
+    string path = "C:\\Users\\User\\OneDrive\\Рабочий стол\\семестровка\\250.txt";
+    reader.open(path, ios_base::in);
+
+    for (int i = 0; i < V; ++i)
+        for (int j = 0; j < V; ++j)
+            reader >> graph[i][j];
+
+    cout << "Max Flow: " << fordFulkerson(graph, 0, 5)<< endl;
+}
