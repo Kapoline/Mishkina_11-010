ed7a31e451b3b6ff390f042339c2e51e3d2b7c0f
Polina <80746085+Kapoline@users.noreply.github.com>
2021-10-02 19:03:03 +0300

    Add files via upload

---
+++ WebApplication1/Instagram/fwer.cs
@@ -0,0 +1,30 @@
+﻿using MimeKit;
+using MailKit.Net.Smtp;
+using System.Threading.Tasks;
+namespace WebApplication1
+{
+    public class EmailService
+    {
+        public static async Task SendEmailAsync(string email, string subject, string message)
+        {
+            var emailMessage = new MimeMessage();
+ 
+            emailMessage.From.Add(new MailboxAddress("Администрация сайта", "polina.chit@mail.ru"));
+            emailMessage.To.Add(new MailboxAddress("", email));
+            emailMessage.Subject = subject;
+            emailMessage.Body = new TextPart(MimeKit.Text.TextFormat.Html)
+            {
+                Text = message
+            };
+             
+            using (var client = new SmtpClient())
+            {
+                await client.ConnectAsync("smtp.yandex.ru", 25, false);
+                await client.AuthenticateAsync("polina.chit@mail.ru", "FaThAnDsOnS18");
+                await client.SendAsync(emailMessage);
+ 
+                await client.DisconnectAsync(true);
+            }
+        }
+    }
+}
